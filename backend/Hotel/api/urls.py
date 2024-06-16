623e5164cfc226fd069b8950c735b8ead9013a91
Bro-Rahul <mr.rahul.0619@gmail.com>
2024-06-16 10:19:33 +0530

    updated dependency file !

--- backend/Hotel/api/urls.py
+++ backend/Hotel/api/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('hotel',views.HotelView.as_view()),
     path('hotel/room',views.HotelRoomsView.as_view()),
-    path('hotel/room/booking',views.BookingView.as_view()),
+    path('booked-history',views.BookingView.as_view()),
     path('reviews',views.ReviewsView.as_view()),
     path('auth',views.AuthenticationView.as_view()),
     path('create/admin',views.AdminUsersView.as_view()),
@@ -15,5 +15,5 @@ urlpatterns = [
     path('hotel/room/<int:id>',views.HotelRoomsView.as_view()),
     path('hotel/<int:id>',views.HotelView.as_view()),
     path('reviews/<int:id>',views.ReviewsView.as_view()),
-    #path('api-auth',include('rest_framework.urls',namespace="api")),
+    path('api-auth',include('rest_framework.urls',namespace="api")),
 ]
